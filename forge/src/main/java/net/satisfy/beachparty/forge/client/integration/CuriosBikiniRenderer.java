1e5ae86de963b3ff79a46592e5e04028cfe36437
satisfy <104036234+satisfyu@users.noreply.github.com>
2025-03-02 23:32:50 +0100

    feat(bikini): DyeColor, renderer

--- forge/src/main/java/net/satisfy/beachparty/forge/client/integration/CuriosBikiniRenderer.java
+++ forge/src/main/java/net/satisfy/beachparty/forge/client/integration/CuriosBikiniRenderer.java
@@ -11,12 +11,12 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.satisfy.beachparty.client.model.BikiniModel;
+import net.satisfy.beachparty.core.item.DyeableBeachpartyArmorItem;
 import net.satisfy.beachparty.core.util.BeachpartyIdentifier;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.client.ICurioRenderer;
 
 public class CuriosBikiniRenderer implements ICurioRenderer {
-
     private final BikiniModel<LivingEntity> model;
     private final ResourceLocation texture;
 
@@ -27,20 +27,24 @@ public class CuriosBikiniRenderer implements ICurioRenderer {
 
     @Override
     public <T extends LivingEntity, M extends EntityModel<T>> void render(ItemStack stack, SlotContext slotContext, PoseStack matrixStack, RenderLayerParent<T, M> renderLayerParent, MultiBufferSource renderTypeBuffer, int light, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
-
+        if (stack.isEmpty() || !(stack.getItem() instanceof DyeableBeachpartyArmorItem)) return;
+        if (stack.hasTag()) {
+            assert stack.getTag() != null;
+            if (stack.getTag().contains("Visible") && !stack.getTag().getBoolean("Visible")) return;
+        }
         LivingEntity livingEntity = slotContext.entity();
         if (livingEntity == null) return;
-
+        int colorInt = ((DyeableBeachpartyArmorItem) stack.getItem()).getColor(stack);
+        float red = ((colorInt >> 16) & 0xFF) / 255f;
+        float green = ((colorInt >> 8) & 0xFF) / 255f;
+        float blue = (colorInt & 0xFF) / 255f;
         model.setupAnim(livingEntity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
-
         matrixStack.pushPose();
-        renderColoredCutoutModel(model, texture, matrixStack, renderTypeBuffer, light, livingEntity, 0.0f, 0.0f, 1.0f);
+        renderColoredCutoutModel(model, texture, matrixStack, renderTypeBuffer, light, livingEntity, red, green, blue);
         matrixStack.popPose();
     }
 
-    private static <T extends LivingEntity, M extends EntityModel<T>> void renderColoredCutoutModel(M model, ResourceLocation texture, PoseStack poseStack, MultiBufferSource buffer, int light, T entity, float red, float green, float blue) {
-        poseStack.pushPose();
+    private static <T extends LivingEntity, M extends EntityModel<T>> void renderColoredCutoutModel(BikiniModel<LivingEntity> model, ResourceLocation texture, PoseStack poseStack, MultiBufferSource buffer, int light, T entity, float red, float green, float blue) {
         model.renderToBuffer(poseStack, buffer.getBuffer(RenderType.entityCutoutNoCull(texture)), light, OverlayTexture.NO_OVERLAY, red, green, blue, 1.0f);
-        poseStack.popPose();
     }
 }
