2a46c8e3efd0c80dc29cdd17641e5454f5d779b4
jason13official <jason13official@gmail.com>
2025-03-02 12:18:36 -0500

    color and render trunks with dyed context

--- forge/src/main/java/net/satisfy/beachparty/forge/client/integration/CuriosTrunksRenderer.java
+++ forge/src/main/java/net/satisfy/beachparty/forge/client/integration/CuriosTrunksRenderer.java
@@ -8,12 +8,20 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
 import net.satisfy.beachparty.client.model.TrunksModel;
+import net.satisfy.beachparty.core.item.DyeableBeachpartyArmorItem;
+import net.satisfy.beachparty.core.registry.ObjectRegistry;
 import net.satisfy.beachparty.core.util.BeachpartyIdentifier;
+import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.client.ICurioRenderer;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class CuriosTrunksRenderer implements ICurioRenderer {
 
@@ -26,24 +34,34 @@ public class CuriosTrunksRenderer implements ICurioRenderer {
     }
 
     @Override
-    public <T extends LivingEntity, M extends EntityModel<T>> void render(ItemStack stack, SlotContext slotContext, PoseStack matrixStack, RenderLayerParent<T, M> renderLayerParent, MultiBufferSource renderTypeBuffer, int light, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+    public <T extends LivingEntity, M extends EntityModel<T>> void render(ItemStack stack, SlotContext slotContext, PoseStack poseStack, RenderLayerParent<T, M> renderLayerParent, MultiBufferSource buffer, int light, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
 
-        LivingEntity livingEntity = slotContext.entity();
-        if (livingEntity == null || stack.isEmpty()) return;
-
+        LivingEntity entity = slotContext.entity();
+        if (entity == null || stack.isEmpty()) return;
         if (!slotContext.identifier().equals("body")) return;
+        model.setupAnim(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
 
-        model.setupAnim(livingEntity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+        AtomicReference<ItemStack> trunks = new AtomicReference<ItemStack>(ItemStack.EMPTY);
 
-        matrixStack.pushPose();
-        renderColoredCutoutModel(model, texture, matrixStack, renderTypeBuffer, light, livingEntity, 0.0f, 0.0f, 1.0f);
-        matrixStack.popPose();
-    }
+        AtomicBoolean inCurioSlot = new AtomicBoolean(false);
+        CuriosApi.getCuriosInventory(entity).ifPresent(curios -> {
+            if (curios.isEquipped(ObjectRegistry.TRUNKS.get())) inCurioSlot.set(true);
+            if (inCurioSlot.get()) trunks.set(curios.findFirstCurio(stackX -> stackX.is(ObjectRegistry.TRUNKS.get())).get().stack());
+        });
 
+        boolean inLegsSlot = entity.getItemBySlot(EquipmentSlot.LEGS).is(ObjectRegistry.TRUNKS.get());
+        if (inLegsSlot) trunks.set(entity.getItemBySlot(EquipmentSlot.LEGS));
 
-    private static <T extends LivingEntity, M extends EntityModel<T>> void renderColoredCutoutModel(M model, ResourceLocation texture, PoseStack poseStack, MultiBufferSource buffer, int light, T entity, float red, float green, float blue) {
+        if (!inCurioSlot.get() && !inLegsSlot) return;
+
+        DyeableBeachpartyArmorItem item = trunks.get().getItem() instanceof DyeableBeachpartyArmorItem ? (DyeableBeachpartyArmorItem) trunks.get().getItem() : null;
+        if (item == null) return;
+
+        DyeColor retrieved = DyeColor.getColor(trunks.get());
+        DyeColor dyeColor = retrieved == null ? DyeColor.BLUE : retrieved;
+
         poseStack.pushPose();
-        model.renderToBuffer(poseStack, buffer.getBuffer(RenderType.entityCutoutNoCull(texture)), light, OverlayTexture.NO_OVERLAY, red, green, blue, 1.0f);
+        model.renderToBuffer(poseStack, buffer.getBuffer(RenderType.entityCutoutNoCull(texture)), light, OverlayTexture.NO_OVERLAY, dyeColor.getTextureDiffuseColors()[0], dyeColor.getTextureDiffuseColors()[1], dyeColor.getTextureDiffuseColors()[2], 1.0f);
         poseStack.popPose();
     }
 }
--- forge/src/main/java/net/satisfy/beachparty/forge/client/renderer/player/layers/TrunksLayer.java
+++ forge/src/main/java/net/satisfy/beachparty/forge/client/renderer/player/layers/TrunksLayer.java
@@ -7,11 +7,19 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.satisfy.beachparty.client.model.BikiniModel;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.ItemStack;
 import net.satisfy.beachparty.client.model.TrunksModel;
+import net.satisfy.beachparty.core.item.DyeableBeachpartyArmorItem;
+import net.satisfy.beachparty.core.registry.ObjectRegistry;
 import net.satisfy.beachparty.core.util.BeachpartyIdentifier;
 import org.antlr.v4.runtime.misc.NotNull;
+import top.theillusivec4.curios.api.CuriosApi;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class TrunksLayer<T extends LivingEntity, M extends HumanoidModel<T>> extends RenderLayer<T, M> {
 
@@ -25,8 +33,28 @@ public class TrunksLayer<T extends LivingEntity, M extends HumanoidModel<T>> ext
     @Override
     public void render(@NotNull PoseStack poseStack, MultiBufferSource multiBufferSource, int i, T entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
         this.model.setupAnim(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+
+        AtomicReference<ItemStack> trunks = new AtomicReference<ItemStack>(ItemStack.EMPTY);
+
+        AtomicBoolean inCurioSlot = new AtomicBoolean(false);
+        CuriosApi.getCuriosInventory(entity).ifPresent(curios -> {
+            if (curios.isEquipped(ObjectRegistry.TRUNKS.get())) inCurioSlot.set(true);
+            if (inCurioSlot.get()) trunks.set(curios.findFirstCurio(stack -> stack.is(ObjectRegistry.TRUNKS.get())).get().stack());
+        });
+
+        boolean inLegsSlot = entity.getItemBySlot(EquipmentSlot.LEGS).is(ObjectRegistry.TRUNKS.get());
+        if (inLegsSlot) trunks.set(entity.getItemBySlot(EquipmentSlot.LEGS));
+
+        if (!inCurioSlot.get() && !inLegsSlot) return;
+
+        DyeableBeachpartyArmorItem item = trunks.get().getItem() instanceof DyeableBeachpartyArmorItem ? (DyeableBeachpartyArmorItem) trunks.get().getItem() : null;
+        if (item == null) return;
+
+        DyeColor retrieved = DyeColor.getColor(trunks.get());
+        DyeColor dyeColor = retrieved == null ? DyeColor.BLUE : retrieved;
+
         poseStack.pushPose();
-        renderColoredCutoutModel(this.model, getTextureLocation(entity), poseStack, multiBufferSource, i, entity, 0.0f, 0.0f, 1.0f);
+        renderColoredCutoutModel(this.model, getTextureLocation(entity), poseStack, multiBufferSource, i, entity, dyeColor.getTextureDiffuseColors()[0], dyeColor.getTextureDiffuseColors()[1], dyeColor.getTextureDiffuseColors()[2]);
         poseStack.popPose();
     }
 
